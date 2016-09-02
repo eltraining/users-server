6b196bf6663186512c3d8f7b1d8729b66d4f2d54
Babst Timo <timo.babst@gmail.com>
2016-09-02 14:16:27 +0200

    simple user with login

--- src/main/java/hello/User.java
+++ src/main/java/hello/User.java
@@ -4,4 +4,36 @@ package hello;
  * Created by TBB on 02.09.2016.
  */
 public class User {
+
+    private final long id;
+    private final String name;
+    private final String password;
+    private final String group;
+
+    public User(long id, String name, String password, String group) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.group = group;
+    }
+
+    public boolean isMe(String name, String password){
+        return this.name.equals(name) && this.password.equals(password);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGroup() {
+        return group;
+    }
 }
--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -3,5 +3,33 @@ package hello;
 /**
  * Created by TBB on 02.09.2016.
  */
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private static User notExisiting = new User(0,"", "", "");
+    private static User onlyUser = new User(1,"Bob", "Morane", "ELCA");
+
+    private static final String noParams = "Hello! Please login!";
+    private static final String known = "Hello, %s! You are logged in!";
+    private static final String unknown = "Login failed!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/login")
+    public Greeting login(@RequestParam(value="name", defaultValue="") String name, @RequestParam(value="password", defaultValue="") String password) {
+
+        if(notExisiting.isMe(name, password)){
+            return new Greeting(counter.incrementAndGet(),noParams);
+        }
+        else if(onlyUser.isMe(name, password)){
+            return new Greeting(counter.incrementAndGet(),String.format(known,name));
+        }
+        else
+        return new Greeting(counter.incrementAndGet(),unknown);
+    }
 }
